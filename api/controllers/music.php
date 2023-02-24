6c6a75941f31c74a23bd833ef2c38cb515df8b89
josephNTJB <86062130+josephNTJB@users.noreply.github.com>
2023-02-24 16:53:40 +0100

    music and playlist controls

--- api/controllers/music.php
+++ api/controllers/music.php
@@ -1,7 +1,44 @@
 <?php
 include_once("./api/utils/import.php");
 
-    
 
+header('Content-Type: application/json');
+$musicServ = new MusicServ();
+if($method == "GET"){
+    if($id){
+    // www.domain.com?controllers=music&method=GET&id=:id
+    // musique par id
+    echo json_encode(array("response" => $musicServ->getById($id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($term){
+    // www.domain.com?controllers=music&method=GET&term=:term
+    // musique par nom
+    echo json_encode(array("response" => $musicServ->findByNameOrArtist($name), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($wanted && $wanted=="liked"){
+        // www.domain.com?controllers=music&method=GET&wanted=liked
+        // musique par nom
+        echo json_encode(array("response" => $musicServ->getLikeSong(/* à verifier je pense à gerer avec la session*/$user_id), "HttpCode" => 200, "datetime" => new datetime()));
+        }
+    else{
+    // www.domain.com?controllers=music&method=GET
+    // liste des musiques likées
+    echo json_encode(array("response" => $musicServ->getAll(), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+}else if ($method == "UPDATE"){
+    if($action && $action=="like"){
+    // www.domain.com?controllers=music&method=UPDATE&id=:id&action=like
+    // liker une musique
+    echo json_encode(array("response" => $musicServ->LikeSong($id,$user_id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($action && $action=="unlike"){
+    // www.domain.com?controllers=music&method=UPDATE&id=:id&action=unlike
+    // unlike une musique
+    echo json_encode(array("response" => $musicServ->unLikeSong($id,$user_id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
 
-?>
+}
+
+//echo json_encode(  $playlistServ->add("Rode", 1) , JSON_PRETTY_PRINT);
+
+?> 
--- api/controllers/playlist.php
+++ api/controllers/playlist.php
@@ -2,4 +2,58 @@
 include_once("./api/utils/import.php");
 
 
-?>
+header('Content-Type: application/json');
+$playlistServ = new PlaylistServ();
+
+if($method == "GET"){
+    if($id){
+    // www.domain.com?controllers=playlist&method=GET&id=:id
+    // playlist par id
+    echo json_encode(array("response" => $playlistServ->getById($id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($term){
+    // www.domain.com?controllers=playlist&method=GET&term=:term
+    // playlist par nom
+    echo json_encode(array("response" => $playlistServ->getByName($name), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($wanted && $wanted=="playlist"){
+        // www.domain.com?controllers=playlist&method=GET&wanted=playlist
+        // playlist de l'utilisateur
+        echo json_encode(array("response" => $playlistServ->getByOwner($ID), "HttpCode" => 200, "datetime" => new datetime()));
+        }
+
+}else if ($method == "UPDATE"){
+    
+    if($action && $action=="like"){
+    // www.domain.com?controllers=playlist&method=UPDATE&id=:id&idMusic=:idMusic&action=remove
+    // liker une musique
+    echo json_encode(array("response" => $playlistServ->LikeSong($id,$user_id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+    else if($action && $action=="remove"){
+    // www.domain.com?controllers=playlist&method=UPDATE&id=:id&action=unlike
+    // retirer une musique de la playlist
+    echo json_encode(array("response" => $playlistServ->removeSong($id,$user_id), "HttpCode" => 200, "datetime" => new datetime()));
+    }
+
+}else if ($method == "POST"){/*
+    public function addSong(int $music_id, int $playlist_id){
+        return $this->playlistRepo->addSongIntoPlaylist($music_id, $playlist_id);
+    }
+
+    public function removeSong(int $music_id, int $playlist_id){
+        return $this->playlistRepo->removeSongFromPlaylist($music_id, $playlist_id);
+    }
+
+    public function add(string $name, int $user_id){
+        $playlist = $this->playlistRepo->save($name, $user_id);
+        return $playlist == null ? null : $playlist->json();
+    }
+
+    public function update(string $name, int $playlist_id){
+        return $this->playlistRepo->update($name, $playlist_id);
+    }*/
+}
+
+//echo json_encode(  $playlistServ->add("Rode", 1) , JSON_PRETTY_PRINT);
+
+?> 
