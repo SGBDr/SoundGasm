f78a6a51691df36355588b281ea742b13d5c4fbc
Kengoum Gassam <kengoumgassam@gmail.com>
2023-02-27 14:41:11 +0100

    ee

---
+++ api/docs/modals/auth-modal.php
@@ -0,0 +1,8 @@
+<div class="modal fade auth-modal" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <h3>Auth Controller</h3>
+            <code></code>
+        </div>
+    </div>
+</div>
