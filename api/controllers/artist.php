3e20878c55f11e4e0e3ce28bfd1e5d51c50bb055
josephNTJB <86062130+josephNTJB@users.noreply.github.com>
2023-02-27 16:04:16 +0100

    added controllers for playlist & music

--- api/controllers/artist.php
+++ api/controllers/artist.php
@@ -9,13 +9,13 @@
             echo json_encode(array("response" => $artistServ->getAll(), "HttpCode" => 200, "datetime" => new datetime()));
         //www.domain.com?controllers=artist&method=GET&all=false
         if($params_p["all"] == "false")
-            echo json_encode(array("response" => $artistServ->getPreference(), "HttpCode" => 200, "datetime" => new datetime()));
+            echo json_encode(array("response" => $artistServ->getPreference($ID), "HttpCode" => 200, "datetime" => new datetime()));
     }else if ($method == "UPDATE"){
         if(explode("=", $params[2])[0] == "for" && explode("=", $params[2])[1] == "ADD_PREF"){
             // www.domain.com?controllers=artist&method=POST&for=ADD_PREF&artist_id=:artist_id
             $artist_id = $params_p["artist_id"];
             $user_id = $ID;
-            echo json_encode(array("response" => array{"add" => $artistServ->addPreference($user_id, $artist_id)}, "HttpCode" => 200, "datetime" => new datetime()));
+            echo json_encode(array("response" => array("add" => $artistServ->addPreference($user_id, $artist_id)), "HttpCode" => 200, "datetime" => new datetime()));
         }else{
             // www.domain.com?controllers=artist&method=DELETE&for=REMOVE_PREF&artist_id=:artist_id
             $artist_id = $params_p["artist_id"];
